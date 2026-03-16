f6d127609b49db127c0b2d49564e4eef466d2cfc
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-16 19:29:26 +0300

    Update get_stream_subjects.php

--- endpoints/get_stream_subjects.php
+++ endpoints/get_stream_subjects.php
@@ -3,7 +3,7 @@ include('../includes/db.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['class_id'])) {
   $class_id = $_POST['class_id'];
-  $stmt = $conn->prepare("SELECT stream FROM classes WHERE id = ?");
+  $stmt = $conn->prepare("SELECT stream FROM classes WHERE class_id = ?");
   $stmt->bind_param("i", $class_id);
   $stmt->execute();
   $stmt->bind_result($stream);
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['class_id'])) {
 
   // Get subjects
   $subjects = [];
-  $stmt = $conn->prepare("SELECT id, subject_name FROM subjects WHERE class_id = ?");
+  $stmt = $conn->prepare("SELECT s.subject_id, s.name FROM subjects s JOIN class_subjects cs ON cs.subject_id = s.subject_id WHERE cs.class_id = ?");
   $stmt->bind_param("i", $class_id);
   $stmt->execute();
   $result = $stmt->get_result();
