da1220a05ff8340a9545fd7689f2e651174d0241
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-23 17:28:05 +0300

    Update get_streams_by_level.php

--- get_streams_by_level.php
+++ get_streams_by_level.php
@@ -3,11 +3,11 @@ include 'includes/db.php';
 
 if (isset($_GET['level'])) {
     $level = $conn->real_escape_string($_GET['level']);
-    $result = $conn->query("SELECT id, stream FROM classes WHERE class_level = '$level' ORDER BY stream");
+    $result = $conn->query("SELECT class_id, stream FROM classes WHERE class_level = '$level' ORDER BY stream");
 
     echo "<option value=''>--Select Stream--</option>";
     while ($row = $result->fetch_assoc()) {
-        echo "<option value='{$row['id']}'>{$row['stream']}</option>";
+        echo "<option value='{$row['class_id']}'>{$row['stream']}</option>";
     }
 }
 ?>
