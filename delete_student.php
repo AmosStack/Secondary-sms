ff095edb9b884d525933db99ee1589ed722263f3
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-24 20:01:37 +0300

    Update delete_student.php

--- delete_student.php
+++ delete_student.php
@@ -1,12 +1,13 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "mafiga");
+include 'includes/db.php';
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+if ($id > 0) {
+    $stmt = $conn->prepare('DELETE FROM students WHERE student_id = ?');
+    $stmt->bind_param('i', $id);
+    $stmt->execute();
+    $stmt->close();
 }
 
-$id = intval($_GET['id']);
-$conn->query("DELETE FROM students WHERE id = $id");
-
-header("Location: view_students.php");
+header('Location: view_students.php');
 exit;
